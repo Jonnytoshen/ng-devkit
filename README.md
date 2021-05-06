c54f3d06d65f006282c8d12214730bc8d672dfc3
Jonny <jonnytoshen@gmail.com>
2021-05-06 14:31:07 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Angular Dev Kit
+
+This is a Angular project, includes tools, functions, utils etc. for developing Angular applications.
+
+## Packages
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ndk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'ng-devkit';
+}
